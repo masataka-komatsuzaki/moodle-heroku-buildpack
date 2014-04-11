8fa21d0a46eff731075718a5d638847046a20eab
広能 章司 <hirono@hiroshinou-shoutsukasa-no-MacBook-Pro.local>
2014-04-11 19:19:53 +0900

    vulcan.sh

--- build/vulcan.sh
+++ build/vulcan.sh
@@ -38,7 +38,7 @@ curl -L ${APACHE_MIRROR_HOST}/apr/apr-util-1.5.3.tar.gz -o /tmp/apr-util-1.5.3.t
 echo "downloading httpd"
 curl -L ${APACHE_MIRROR_HOST}/httpd/httpd-2.4.9.tar.gz -o /tmp/httpd-2.4.9.tar.gz
 echo "downloading php"
-curl -L http://us.php.net/get/php-5.5.11.tar.gz/from/us2.php.net/mirror -o /tmp/#php-5.5.11.tar.gz
+curl -L http://us.php.net/get/php-5.5.11.tar.gz/from/us2.php.net/mirror -o /tmp/php-5.5.11.tar.gz
 echo "downloading pecl-memcached"
 curl -L http://pecl.php.net/get/memcached-2.1.0.tgz -o /tmp/memcached-2.1.0.tgz
 echo "download zlib"
@@ -118,11 +118,11 @@ ${MAKE} && ${MAKE} install
 
 cd /tmp/memcached-2.1.0
 /app/php/bin/phpize
-./configure --with-libmemcached-dir=/app/local \
-  --prefix=/app/php \
-  --enable-memcached-igbinary \
-  --enable-memcached-json \
-  --with-php-config=/app/php/bin/php-config \
+./configure --with-libmemcached-dir=/app/local ¥
+  --prefix=/app/php ¥
+  --enable-memcached-igbinary ¥
+  --enable-memcached-json ¥
+  --with-php-config=/app/php/bin/php-config ¥
   --enable-static
 ${MAKE} && ${MAKE} install
 
